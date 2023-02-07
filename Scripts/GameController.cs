257097a70477613d468431de3139f90c8bc1ab44
Banyapon Poolsawas <daydevthailand@gmail.com>
2023-02-07 14:47:10 +0700

    Fixed Model Replace

--- Scripts/GameController.cs
+++ Scripts/GameController.cs
@@ -4,18 +4,31 @@ using UnityEngine;
 
 public class GameController : MonoBehaviour
 {
-    public GameObject RotatePivot;
-    public string mainValue;
+    public GameObject RotatePivot, main_canvas;
+    public string mainValue,newValue;
 
-    public void getObject(string value){
-        GameObject instance = Instantiate(Resources.Load("model/"+value, typeof (GameObject))) as GameObject;
+    public void getObject(string value)
+    {
+        newValue = value;
+        if(newValue != mainValue)
+        {
+
+            Debug.Log(mainValue);
+            GameObject oldModel;
+            oldModel = GameObject.Find(mainValue+ "(Clone)");
+            Destroy(oldModel);
+        }
+        GameObject instance = Instantiate(Resources.Load("model/" + value, typeof(GameObject))) as GameObject;
         instance.transform.parent = RotatePivot.transform;
         mainValue = value;
     }
-    public void loadInfo(){
-        GameObject instance_image = Instantiate(Resources.Load("images/"+mainValue, typeof(GameObject))) as GameObject;
+
+    public void loadInfo()
+    {
+        GameObject instance_image = Instantiate(Resources.Load("images/" + mainValue, typeof(GameObject))) as GameObject;
+        instance_image.transform.parent = main_canvas.transform;
     }
-    
+
     public void loadSound()
     {
         AudioSource sndSource = gameObject.AddComponent<AudioSource>() as AudioSource;
